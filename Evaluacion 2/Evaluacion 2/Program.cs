6b5a18aaec32db285f1702002c381d7d9bfe6dc8
juanptorop <78567708+juanptorop@users.noreply.github.com>
2021-02-27 22:57:09 -0400

    Add files via upload

---
+++ Evaluacion 2/Evaluacion 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Evaluacion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "";
+            int edad = 0;
+            double altura = 0.0;
+
+
+            Console.WriteLine("Bienvenido, por favor ingrese su edad");
+            cadena = Console.ReadLine();
+            edad = Convert.ToInt32(cadena);
+            if (edad >= 16)
+            {
+                Console.WriteLine("Lamentablemente usted ha superado el limite de edad para el parque :(");
+            }
+            else
+            {
+
+              Console.WriteLine("Bienvenido");
+
+                Console.WriteLine("¿cual es tu altura? En metros");
+                cadena = Console.ReadLine();
+                altura = Convert.ToDouble(cadena);
+
+                if (altura < 1.55)
+                {
+                    Console.WriteLine("Puedes subir a todas las atracciones salvo las montañas rusas");
+                }
+                else
+                {
+                    Console.WriteLine("¡Felicidades! ¡Puedes subir a todas las atracciones");
+
+                }
+                
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}   
